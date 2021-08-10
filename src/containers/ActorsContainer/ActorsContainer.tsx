f9938e869bb4ae62b776d469067fb86e2e04ed1a
art <amovsisyan0@gmail.com>
2021-08-10 20:47:07 +0400

    initial commit

---
+++ src/containers/ActorsContainer/ActorsContainer.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {ActorsComponent} from "../../components/ActorsComponent/ActorsComponent";
+
+export const ActorsContainer = () => {
+    return (
+        <ActorsComponent />
+    );
+}
---
+++ src/containers/LoginContainer/LoginContainer.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {LoginComponent} from "../../components/LoginComponent/LoginComponent";
+
+export const LoginContainer = () => {
+    return (
+        <LoginComponent />
+    );
+}
