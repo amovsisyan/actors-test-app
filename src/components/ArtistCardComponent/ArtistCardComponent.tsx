5789a6a9daf2869fba4b7c3945f76e7313ac023b
art <amovsisyan0@gmail.com>
2021-08-11 21:52:17 +0400

    image preview

--- src/components/ArtistCardComponent/ArtistCardComponent.tsx
+++ src/components/ArtistCardComponent/ArtistCardComponent.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Card, Rate} from "antd";
+import {Card, Rate, Image} from "antd";
 import {IArtist} from "../../services/api/SpotifyApi";
 import noImage from "../../images/no-image.png";
 import './ArtistCardComponent.css';
@@ -12,6 +12,14 @@ interface IActorsComponent {
 
 export const ArtistCardComponent = (props: IActorsComponent) => {
     const {artist} = props;
+    const [visible, setVisible] = React.useState(false);
+    const hasAtLeastOneImage = artist.images[0];
+
+    const onClickHandler = () => {
+        if (hasAtLeastOneImage) {
+            setVisible(true)
+        }
+    }
 
     return (
         <>
@@ -19,11 +27,21 @@ export const ArtistCardComponent = (props: IActorsComponent) => {
                 className={'img_wrapper'}
                 hoverable
                 style={{width: 240}}
-                cover={<img src={artist.images[0] ? artist.images[0].url : noImage}/>}
+                cover={<Image
+                    src={hasAtLeastOneImage ? artist.images[1].url : noImage}
+                    preview={{ visible: false }}
+                    onClick={onClickHandler}
+                />}
             >
                 <Meta title={artist.name} description={`Followers: ${artist.followers.total}`}/>
                 <Rate allowHalf defaultValue={artist.popularity / 20} />
             </Card>
+
+            <div style={{ display: 'none' }}>
+                <Image.PreviewGroup preview={{ visible, onVisibleChange: vis => setVisible(vis) }}>
+                    <Image src={hasAtLeastOneImage ? artist.images[0].url : noImage} />
+                </Image.PreviewGroup>
+            </div>
         </>
     )
 }
