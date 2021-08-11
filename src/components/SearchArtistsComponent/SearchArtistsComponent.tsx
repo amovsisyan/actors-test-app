a8be9a72757d9b87bea04ffe461a53290dc68138
art <amovsisyan0@gmail.com>
2021-08-11 22:12:12 +0400

    use debounce for searching

--- src/components/SearchArtistsComponent/SearchArtistsComponent.tsx
+++ src/components/SearchArtistsComponent/SearchArtistsComponent.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {ChangeEventHandler} from 'react';
 import {Col, Row, Input} from "antd";
 import {IArtist} from "../../services/api/SpotifyApi";
 import './SearchArtistsComponent.css';
@@ -8,17 +8,19 @@ const {Search} = Input;
 
 interface IActorsComponent {
     artists: IArtist[]
+    searchValue: string
     onSearch: (value: string) => void
+    onChange: ChangeEventHandler<HTMLInputElement>
 }
 
 export const SearchArtistsComponent = (props: IActorsComponent) => {
-    const {artists, onSearch} = props;
+    const {artists, searchValue, onSearch, onChange} = props;
 
     return (
         <>
             <Row justify="center" align='middle'>
                 <Col className={'col_wrapper'}>
-                    <Search placeholder="Search for an artist..." onSearch={onSearch} style={{width: 200}}/>
+                    <Search placeholder="Search for an artist..." onSearch={onSearch} onChange={onChange} value={searchValue} style={{width: 200}}/>
                 </Col>
             </Row>
             <ArtistsComponent artists={artists} />
--- src/containers/SearchArtistsContainer/SearchArtistsContainer.tsx
+++ src/containers/SearchArtistsContainer/SearchArtistsContainer.tsx
@@ -1,20 +1,47 @@
-import React, {useState} from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import {SearchArtistsComponent} from "../../components/SearchArtistsComponent/SearchArtistsComponent";
 import {getArtists, IArtist} from "../../services/api/SpotifyApi";
+import useDebounce from "../../hooks/useDebounce";
+
+const DEBOUNCE_DELAY = 500;
 
 export const SearchArtistsContainer = () => {
+    const [searchValue, setSearchValue] = useState<string>('')
     const [artists, setArtists] = useState<IArtist[]>([])
 
-    const onSearchHandler = (value: string) => {
+    const setArtistsRequest = (value: string) => {
         getArtists(value).then((res) => {
             setArtists(res.data.artists.items);
         })
     }
 
+    const onSearchHandler = (value: string) => {
+        setArtistsRequest(value)
+    }
+
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setSearchValue(e.target.value)
+    }
+
+    const debouncedSearchTerm = useDebounce<string>(searchValue, DEBOUNCE_DELAY);
+
+    React.useEffect(
+        () => {
+            if (debouncedSearchTerm) {
+                setArtistsRequest(debouncedSearchTerm)
+            } else {
+                setArtists([]);
+            }
+        },
+        [debouncedSearchTerm]
+    );
+
     return (
         <SearchArtistsComponent
             artists={artists}
+            searchValue={searchValue}
             onSearch={onSearchHandler}
+            onChange={onChangeHandler}
         />
     );
 }
